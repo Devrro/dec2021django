1b9d5b9b5091c36a65ea087b4fcc75a5a4ebfc7c
Serhii_Terletckiy <beinght@gmail.com>
2022-06-29 21:22:30 +0300

    PcShop done

--- apps/cars/views.py
+++ apps/cars/views.py
@@ -1,7 +1,5 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.views import APIView
-from rest_framework.mixins import CreateModelMixin, ListModelMixin
-from rest_framework.response import Response
+
 
 # Create your views here.
 from .models import CarModel
@@ -9,6 +7,26 @@ from .serializer import CarSerializer
 
 
 class CarListCreateView(ListCreateAPIView):
+    serializer_class = CarSerializer
+    queryset = CarModel.objects.all()
+
+    def get_queryset(self):
+        qs = self.queryset.all()
+        auto_park_id = self.request.query_params.get('autoParkId')
+        if auto_park_id:
+            qs = qs.filter(auto_parks_id=auto_park_id)
+            print(qs.query)
+        return qs
+
+# class GetAll(APIView):
+#
+#     def get(self, *args,**kwargs):
+#         car = CarModel.objects.all()
+#         serializer = CarSerializer(car,many=True)
+#         return Response(serializer.data)
+
+
+class CarUpdateReadDeleteByIdView(RetrieveUpdateDestroyAPIView):
     queryset = CarModel.objects.all()
     serializer_class = CarSerializer
 
@@ -17,22 +35,5 @@ class CarListCreateView(ListCreateAPIView):
         auto_park_id = self.request.query_params.get('auto_park_id')
         if auto_park_id:
             qs.filter(auto_parks_id=auto_park_id)
-        return super().get_queryset()
-
-class GetAll(APIView):
-
-    def get(self, *args,**kwargs):
-        car = CarModel.objects.all()
-        serializer = CarSerializer(car,many=True)
-        return Response()
-
-class CarUpdateReadDeleteByIdView(RetrieveUpdateDestroyAPIView):
-    queryset = CarModel.objects.all()
-    serializer_class = CarSerializer
-    #
-    # def get(self, *args, **kwargs):
-    #     return super().get(self.request, *args, **kwargs)
-    #
-    # def delete(self, request, *args, **kwargs):
-    #     return super().destroy(request, *args, **kwargs)
+        return qs
 
